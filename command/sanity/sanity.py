18fefaa722ed748dd96d10f28c16ce8ad32150de
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2022-02-02 16:10:04 +0700

    Correct leftover from template plugin

--- command/sanity/sanity.py
+++ command/sanity/sanity.py
@@ -1,14 +1,13 @@
 import lightbulb
 import hikari
 
-from lightbulb import context
-from remi.core.constant import Global
+from lightbulb import commands, context
 
 sanity_plugin = lightbulb.Plugin(name="Sanity checker")
 
 
 @sanity_plugin.command
 @lightbulb.command(name="sanity", description="Check your sanity here!")
-@lightbulb.implements(*Global.command_implements)
+@lightbulb.implements(commands.PrefixCommand, commands.SlashCommand)
 async def sanity_sanity(ctx: context.Context):
     await ctx.respond("Sanity!")
